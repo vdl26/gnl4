77ad38edfe05c6fbb92406732690046140c1580f
Vivian de Lange <vivian@Vivians-MBP-2.domain_not_set.invalid>
2021-06-19 23:32:47 +0200

    sec

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vde-lang <vde-lang@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/29 10:36:13 by vde-lang      #+#    #+#                 */
-/*   Updated: 2021/06/19 12:25:51 by vde-lang      ########   odam.nl         */
+/*   Updated: 2021/06/19 23:31:32 by vde-lang      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int	newline(char *line)
 	return (-1);
 }
 
-int	save_l(char *str, char **line, int i, int fd)
+int	save_l(char *str, char **line, int i)
 {
 	int			len;
 
@@ -55,18 +55,16 @@ int	save_l(char *str, char **line, int i, int fd)
 	++i;
 	len = ft_strlen(str + i) + 1;
 	ft_memmove(str, str + i, len);
-	if (fd == 0)
-		return (0);
 	return (1);
 }
 
-int	ft_remainder(char *savedline, char **line, int ret)
+int	ft_remainder(char **savedline, char **line, int ret)
 {
-	*line = ft_strdup(savedline);
+	*line = ft_strdup(*savedline);
+	free(*savedline);
+	*savedline = NULL;
 	if (*line == NULL)
 		return (-1);
-	free(savedline);
-	savedline = NULL;
 	return (ret);
 }
 
@@ -77,11 +75,14 @@ int	get_next_line(int fd, char **line)
 	int			i;
 	int			ret;
 
-	if (fd < 0 || !line || BUFFER_SIZE < 1)
+	if (fd < 0 || !line || BUFFER_SIZE < 1 || fd > 1024)
+	{
+		*line = NULL;
 		return (-1);
+	}
 	i = newline(savedline[fd]);
 	if (i != -1)
-		return (save_l(savedline[fd], line, i, fd));
+		return (save_l(savedline[fd], line, i));
 	ret = read(fd, buff, BUFFER_SIZE);
 	while (ret > 0)
 	{
@@ -90,11 +91,11 @@ int	get_next_line(int fd, char **line)
 		if (savedline[fd] == NULL)
 			return (-1);
 		if (newline(savedline[fd]) != -1)
-			return (save_l(savedline[fd], line, newline(savedline[fd]), fd));
+			return (save_l(savedline[fd], line, newline(savedline[fd])));
 		ret = read(fd, buff, BUFFER_SIZE);
 	}
 	if (savedline[fd])
-		return (ft_remainder(savedline[fd], line, ret));
-	*line = ft_strdup("");
+		return (ft_remainder(&savedline[fd], line, ret));
+	*line = NULL;
 	return (ret);
 }
